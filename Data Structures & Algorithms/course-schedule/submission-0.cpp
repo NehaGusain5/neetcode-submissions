892990d38b90e2bf0e64e9f8db67a60613ba09a3
Neha Gusain <nehagusain255@gmail.com>
2026-07-17 22:47:39 +0530

    Add: course-schedule - submission-0

---
+++ Data Structures & Algorithms/course-schedule/submission-0.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        int V=numCourses;
+        vector<int> indegree(V,0);
+        vector<vector<int>> adj(V);
+
+        for(auto edge:prerequisites){
+            int u=edge[0];
+            int v=edge[1];
+            adj[v].push_back(u);
+        }
+
+        for(int i=0;i<V;i++){
+            for(auto it:adj[i]){
+                indegree[it]+=1;
+            }
+        }
+
+        queue<int> q;
+        for(int i=0;i<V;i++){
+            if(indegree[i]==0){
+                q.push(i);
+            }
+        }
+        vector<int> topo;
+
+        while(!q.empty()){
+            int node=q.front();
+            q.pop();
+            topo.push_back(node);
+
+            for(auto it:adj[node]){
+                indegree[it]--;
+                if(indegree[it]==0){
+                    q.push(it);
+                }
+            }
+
+        }
+        return topo.size()==V;
+        
+    }
+};
